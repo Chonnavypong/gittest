ffc53cd35985e361a9cffedddb45fb0947812faf
chonnavypong <chonnavypong@gmail.com>
2020-09-29 22:03:33 +0700

    test git

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
+
+require('dotenv').config({
+  path: './config.env'
+})
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -21,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+console.log(process.env.HELLO)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
